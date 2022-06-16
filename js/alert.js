acd198c5bd6a9603daa68326f5e00b6e1856b25d
Fran Ortega <franortega.wg@gmail.com>
2022-06-16 12:49:10 -0300

    platformv1

--- js/alert.js
+++ js/alert.js
@@ -2,6 +2,14 @@ function alerteffect(){
     var alerta = document.getElementById("alert");
     alerta.classList.remove("hidden");
     alerta.classList.add("show");
+    setTimeout(() => {
+        alerta.classList.add("hide");
+        setTimeout(() => {
+            alerta.classList.add("hidden");  
+            alerta.classList.remove("show");
+            alerta.classList.remove("hide");    
+        }, 1500); 
+    }, 3000);
 }
 
 function alertclose(){
